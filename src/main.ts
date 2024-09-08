c4dbf51183dd0ea38eb7f997fc79e2f720dd74e6
Maxime Moreillon <m.moreillon@gmail.com>
2024-09-08 09:38:53 +0900

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,35 @@
 import { registerPlugins } from "@/plugins";
 import App from "./App.vue";
 import { createApp } from "vue";
-import { useKeycloakStore } from "./stores/keycloak";
 import { kcClient } from "@/keycloak";
 
 const app = createApp(App);
 
-console.log("[Keycloak] Init...");
 kcClient
   .init({
     // onLoad: "check-sso", // 'login-required'|'check-sso';
     onLoad: "check-sso",
+    enableLogging: true,
   })
   .then(() => {
-    console.log("[Keycloak] Init OK");
     registerPlugins(app);
-    const keycloakStore = useKeycloakStore();
-    keycloakStore.client = kcClient;
+    // const keycloakStore = useKeycloakStore();
+    // keycloakStore.client = kcClient;
 
     app.mount("#app");
+
+    setInterval(() => {
+      if (kcClient.authenticated) kcClient.updateToken();
+    }, 60000);
   });
 
 //  This approach enforces login prior to using the app
-// console.log(`Keycoak client init...`);
 // keycloakStore.client
 //   .init({
 //     onLoad: "login-required", // 'login-required'|'check-sso';
 //     enableLogging: true,
 //   })
 //   .then((authorized) => {
-//     console.log(`Keycoak client init finished`);
 
 //     if (authorized) app.mount("#app");
 
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -5,22 +5,14 @@ export const beforeEachHandler: NavigationGuardWithThis<undefined> = async (
   to,
   from
 ) => {
-  // const keycloakStore = useKeycloakStore();
-
   console.log(`[Router] before ${to.path}`);
 
   if (to.path.startsWith("/protected")) {
     console.log("Route is protected");
 
-    if (kcClient.authenticated) return;
-
     if (!kcClient.authenticated) {
-      console.warn(`[Keycload] Client is not authenticated`);
-
-      // kcClient.login({
-      //   redirectUri: `${window.location}/${to.path}`,
-      // });
-      kcClient.login();
+      // kcClient.login();
+      // return false;
     }
   }
 
